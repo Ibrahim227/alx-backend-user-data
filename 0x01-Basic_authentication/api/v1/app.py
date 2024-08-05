98f03cb0593ad3d48fa1fef5913a1643d95a5ae5
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-08-05 15:10:12 -0700

    update api/v1/app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,8 +23,14 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unauthorized"""
+    """Unauthorized Handler"""
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Fobidden Handler"""
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
