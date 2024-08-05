50c9135ced8b33977844b9d2031b8279e6a99964
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-08-05 15:31:12 -0700

    update api/v1/auth/auth.py

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Import required Module/Lib"""
+from flask Import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """A class to manage the API authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require Auth
+        Returns:
+          - False - path nd excluded_paths will be used late
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Authorization Header
+        Return:
+          - None - request will be the Flask request object
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current User
+        Return:
+          - None - request will be the Flask request object
+        """
+        return None        
