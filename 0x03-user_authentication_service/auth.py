215ef434f65f041327983d0f897972cf58237cf8
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-08-15 03:58:47 -0700

    update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,4 +4,9 @@ import bcrypt
 
 def _hash_password(password: str) -> bytes:
     """returns bytes"""
-    return bcrypt.hashpw(password)
+    value = password
+    salt = bcrypt.gensalt()
+    bytes = value.encode('utf-8')
+
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
